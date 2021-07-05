d8f596520e74633221c82017aae14d21c062e2a4
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-07-05 21:57:56 +0200

    Add "LinkedListExtraTask" class

---
+++ src/test/java/LinkedListExtraTaskTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LinkedListExtraTaskTest {
+  @Test
+  @DisplayName("Should return zipped list")
+  void shouldReturnZippedList() {
+    LinkedList listA = new LinkedList();
+    LinkedList listB = new LinkedList();
+
+    listA.addInTail(new Node(1));
+    listA.addInTail(new Node(2));
+    listA.addInTail(new Node(3));
+
+    listB.addInTail(new Node(4));
+    listB.addInTail(new Node(5));
+    listB.addInTail(new Node(6));
+
+    LinkedList result = LinkedListExtraTask.zip(listA, listB);
+
+    Assertions.assertEquals(3, result.count());
+    Assertions.assertNotNull(result.find(5));
+    Assertions.assertNotNull(result.find(7));
+    Assertions.assertNotNull(result.find(9));
+  }
+
+  @Test
+  @DisplayName("Should return empty list")
+  void shouldReturnEmptyList() {
+    LinkedList listA = new LinkedList();
+    LinkedList listB = new LinkedList();
+
+    LinkedList result = LinkedListExtraTask.zip(listA, listB);
+
+    Assertions.assertEquals(0, result.count());
+  }
+
+  @Test
+  @DisplayName("Should return empty list")
+  void shouldReturnEmptyList1() {
+    LinkedList listA = new LinkedList();
+    LinkedList listB = new LinkedList();
+
+    listA.addInTail(new Node(1));
+    listA.addInTail(new Node(2));
+
+    listB.addInTail(new Node(4));
+    listB.addInTail(new Node(5));
+    listB.addInTail(new Node(6));
+
+    LinkedList result = LinkedListExtraTask.zip(listA, listB);
+
+    Assertions.assertEquals(0, result.count());
+  }
+}
