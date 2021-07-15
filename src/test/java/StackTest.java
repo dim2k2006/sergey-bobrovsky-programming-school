1110710a4a1fd6392d9609780cbbb4787cf50259
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-07-15 20:59:44 +0200

    Implement "peek" method

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -8,8 +8,8 @@ public class StackTest {
   @DisplayName("push method")
   class PushMethod {
     @Test
-    @DisplayName("Should make array with new capacity")
-    public void shouldMakeArrayWithNewCapacity() {
+    @DisplayName("Should push items to stack")
+    public void shouldPushItemsToStack() {
       Stack<Integer> stack = new Stack<Integer>(Integer.class);
 
       stack.push(1);
@@ -26,6 +26,39 @@ public class StackTest {
 
       Assertions.assertEquals(3, stack.size());
       Assertions.assertEquals(3, stack.peek());
+    }
+  }
+
+  @Nested
+  @DisplayName("peek method")
+  class PeekMethod {
+    @Test
+    @DisplayName("Should return last item from stack")
+    public void shouldReturnLastItemFromStack() {
+      Stack<Integer> stack = new Stack<Integer>(Integer.class);
+
+      stack.push(1);
+
+      int last1 = stack.peek();
+
+      Assertions.assertEquals(1, last1);
+      Assertions.assertEquals(1, stack.size());
+
+      stack.push(2);
+      stack.push(3);
+
+      int last2 = stack.peek();
+
+      Assertions.assertEquals(3, last2);
+      Assertions.assertEquals(3, stack.size());
+    }
+
+    @Test
+    @DisplayName("Should return null if stack is empty")
+    public void shouldReturnNullIfStackIsEmpty() {
+      Stack<Integer> stack = new Stack<Integer>(Integer.class);
+
+      Assertions.assertNull(stack.peek());
     }
   }
 }
