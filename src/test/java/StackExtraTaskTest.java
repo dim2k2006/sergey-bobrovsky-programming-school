4a713e4d1c37d080cdc0d8e8babadf9b68023cdf
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-07-17 21:01:14 +0200

    Update tests

--- src/test/java/StackExtraTaskTest.java
+++ src/test/java/StackExtraTaskTest.java
@@ -3,11 +3,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StackExtraTaskTest {
-  @Test
-  @DisplayName("Should return 59")
-  void shouldReturn59() {
-    int result = StackExtraTask.calculate("8 2 + 5 * 9 +");
+  public String[][] testCases = new String[][]{
+    { "8 2 + 5 * 9 +", "59" },
+    { "13 2 * 10 + 2 *", "72" },
+    { "19 16 + 2 + 8 +", "45" },
+    { "14 8 * 4 * 19 *", "8512" },
+  };
 
-    Assertions.assertEquals(59, result);
+  @Test
+  @DisplayName("Should calculate the expression")
+  void ShouldCalculateTheExpression() {
+    for (String[] testCase : testCases) {
+      int result = StackExtraTask.calculate(testCase[0]);
+
+      Assertions.assertEquals(Integer.parseInt(testCase[1]), result);
+    }
   }
 }
