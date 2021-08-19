cec544f2fcebbdfcb92f1e7ae00129b9af2bbcd1
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-08-19 19:08:37 +0200

    Implement "put" method

--- src/test/java/PowerSetTest.java
+++ src/test/java/PowerSetTest.java
@@ -11,15 +11,49 @@ public class PowerSetTest {
     @Test
     @DisplayName("Should return size of set")
     void shouldReturnSizeOfSet() {
+      Faker faker = new Faker();
       PowerSet set = new PowerSet();
 
       Assertions.assertEquals(0, set.size());
 
-      set.put("a");
-      set.put("b");
-      set.put("c");
+      set.put(faker.lorem().word());
+      set.put(faker.lorem().word());
+      set.put(faker.lorem().word());
 
       Assertions.assertEquals(3, set.size());
+    }
+  }
+
+  @Nested
+  @DisplayName("put method")
+  class PutMethod {
+    @Test
+    @DisplayName("Should put item to set")
+    void shouldPutItemToSet() {
+      Faker faker = new Faker();
+      PowerSet set = new PowerSet();
+
+      Assertions.assertEquals(0, set.size());
+
+      set.put(faker.lorem().word());
+
+      Assertions.assertEquals(1, set.size());
+    }
+
+    @Test
+    @DisplayName("Should not create duplicates in set")
+    void shouldNotCreateDuplicatesInSet() {
+      Faker faker = new Faker();
+      PowerSet set = new PowerSet();
+
+      String value = faker.lorem().word();
+
+      Assertions.assertEquals(0, set.size());
+
+      set.put(value);
+      set.put(value);
+
+      Assertions.assertEquals(1, set.size());
     }
   }
 }
